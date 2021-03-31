726b852ef77166eae1a0695607fc5c8763c9d207
Pedro Henrique Ribeiro <pedro.henrique59@outllook.com>
2021-03-31 16:24:03 -0300

    layout tela recuperar senha

--- app/src/main/java/com/fornadagora/activity/RecuperarSenhaActivity.java
+++ app/src/main/java/com/fornadagora/activity/RecuperarSenhaActivity.java
@@ -1,11 +1,13 @@
 package com.fornadagora.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.fornadagora.R;
 import com.fornadagora.helper.ConfiguracaoFirebase;
@@ -17,6 +19,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class RecuperarSenhaActivity extends AppCompatActivity {
 
     private TextInputEditText campoEmail;
+    private Toolbar toolbar;
 
     private FirebaseAuth autenticacao;
 
@@ -25,10 +28,12 @@ public class RecuperarSenhaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recuperar_senha);
         inicializarComponentes();
+        configurarToolbar();
     }
 
     public void inicializarComponentes() {
         campoEmail = findViewById(R.id.edit_text_recuperar_email);
+        toolbar = findViewById(R.id.toolbarPrincipal);
     }
 
     public void enviarEmailRecuperarSenha(View view) {
@@ -50,5 +55,23 @@ public class RecuperarSenhaActivity extends AppCompatActivity {
         } else {
             Toast.makeText(RecuperarSenhaActivity.this, "Preencha o campo de e-mail", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void configurarToolbar(){
+        toolbar.setTitle("Recuperar senha");
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorPrimary));
+        toolbar.setNavigationIcon(R.drawable.ic_voltar_24);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirTelaLogin();
+            }
+        });
+    }
+
+    public void abrirTelaLogin(){
+        Intent i = new Intent(RecuperarSenhaActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
