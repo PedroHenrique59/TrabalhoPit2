ec80572c911dbf4641864b01d57538952ba139de
Pedro Henrique Ribeiro <pedro.henrique59@outllook.com>
2021-04-15 10:43:31 -0300

    validando se existe conta para enviar e-mail de recuperação senha

--- app/src/main/java/com/fornadagora/activity/RecuperarSenhaActivity.java
+++ app/src/main/java/com/fornadagora/activity/RecuperarSenhaActivity.java
@@ -11,17 +11,30 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.fornadagora.R;
 import com.fornadagora.helper.ConfiguracaoFirebase;
+import com.fornadagora.model.Padaria;
+import com.fornadagora.model.Usuario;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RecuperarSenhaActivity extends AppCompatActivity {
 
     private TextInputEditText campoEmail;
+
     private Toolbar toolbar;
 
     private FirebaseAuth autenticacao;
+
+    private Query queryUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,26 +51,14 @@ public class RecuperarSenhaActivity extends AppCompatActivity {
 
     public void enviarEmailRecuperarSenha(View view) {
         autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
-
         if (!campoEmail.getText().toString().isEmpty()) {
-            autenticacao.sendPasswordResetEmail(campoEmail.getText().toString())
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                campoEmail.setText("");
-                                Toast.makeText(RecuperarSenhaActivity.this, "E-mail enviado com sucesso", Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(RecuperarSenhaActivity.this, "Falha ao enviar o e-mail", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
+            validarExisteConta(campoEmail.getText().toString());
         } else {
             Toast.makeText(RecuperarSenhaActivity.this, "Preencha o campo de e-mail", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void configurarToolbar(){
+    public void configurarToolbar() {
         toolbar.setTitle("Recuperar senha");
         toolbar.setTitleTextColor(getResources().getColor(R.color.colorPrimary));
         toolbar.setNavigationIcon(R.drawable.ic_voltar_24);
@@ -69,9 +70,39 @@ public class RecuperarSenhaActivity extends AppCompatActivity {
         });
     }
 
-    public void abrirTelaLogin(){
+    public void abrirTelaLogin() {
         Intent i = new Intent(RecuperarSenhaActivity.this, MainActivity.class);
         startActivity(i);
         finish();
+    }
+
+    public void validarExisteConta(String email) {
+        queryUsuario = ConfiguracaoFirebase.getFirebase().child("usuarios").orderByChild("email").equalTo(email);
+        queryUsuario.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    autenticacao.sendPasswordResetEmail(campoEmail.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        campoEmail.setText("");
+                                        Toast.makeText(RecuperarSenhaActivity.this, "E-mail enviado com sucesso", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(RecuperarSenhaActivity.this, "Falha ao enviar o e-mail", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                } else {
+                    Toast.makeText(RecuperarSenhaActivity.this, "Não existe nenhuma conta cadastrada para o endereço de e-mail informado.", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
