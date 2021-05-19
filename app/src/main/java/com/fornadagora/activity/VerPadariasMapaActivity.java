25cdf5d7619409be8d5991ac93509e75d1d6b3a7
Pedro Henrique Ribeiro <pedro.henrique59@outllook.com>
2021-05-19 17:45:11 -0300

    alterações no gradle / obtendo localização atual

--- app/src/main/java/com/fornadagora/activity/VerPadariasMapaActivity.java
+++ app/src/main/java/com/fornadagora/activity/VerPadariasMapaActivity.java
@@ -1,13 +1,29 @@
 package com.fornadagora.activity;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.fornadagora.R;
 import com.fornadagora.helper.ConfiguracaoFirebase;
 import com.fornadagora.model.Padaria;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -15,12 +31,18 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 
-public class VerPadariasMapaActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VerPadariasMapaActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -28,17 +50,35 @@ public class VerPadariasMapaActivity extends FragmentActivity implements OnMapRe
 
     private Padaria padaria;
 
+    private EditText campoPesquisa;
+
+    private FusedLocationProviderClient client;
+
+    private SupportMapFragment supportMapFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_padarias_mapa);
+
+        campoPesquisa = findViewById(R.id.editTextBuscar);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        referenciaPadaria = ConfiguracaoFirebase.getFirebase().child("padarias");
+        supportMapFragment = mapFragment;
 
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(VerPadariasMapaActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation();
+        } else {
+            ActivityCompat.requestPermissions(VerPadariasMapaActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+
+        referenciaPadaria = ConfiguracaoFirebase.getFirebase().child("padarias");
         referenciaPadaria.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -53,7 +93,6 @@ public class VerPadariasMapaActivity extends FragmentActivity implements OnMapRe
 
                     }
                 }
-
             }
 
             @Override
@@ -61,6 +100,76 @@ public class VerPadariasMapaActivity extends FragmentActivity implements OnMapRe
 
             }
         });
+
+        inicializar();
+    }
+
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(final Location location) {
+                if (location != null) {
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(@NonNull GoogleMap googleMap) {
+                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            MarkerOptions options = new MarkerOptions().position(latLng).title("Você está aqui");
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+                            mMap.addMarker(options);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 44){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getCurrentLocation();
+            }
+        }
+    }
+
+    private void inicializar() {
+        campoPesquisa.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_ACTION_DONE || event.getAction() == KeyEvent.ACTION_DOWN
+                        || event.getAction() == KeyEvent.KEYCODE_ENTER) {
+                    pesquisarEndereco();
+                }
+                return false;
+            }
+        });
+    }
+
+    private void pesquisarEndereco() {
+        String endereco = campoPesquisa.getText().toString();
+        Geocoder geocoder = new Geocoder(VerPadariasMapaActivity.this);
+        List<Address> list = new ArrayList<>();
+        MarkerOptions markerOptions = new MarkerOptions();
+        try {
+            list = geocoder.getFromLocationName(endereco, 1);
+        } catch (IOException e) {
+
+        }
+        if (list.size() > 0) {
+            Address address = list.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            markerOptions.position(latLng);
+            markerOptions.title("");
+            markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.bakery_icone));
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            mMap.animateCamera(CameraUpdateFactory.zoomBy(12));
+        }
     }
 
     /**
@@ -75,13 +184,5 @@ public class VerPadariasMapaActivity extends FragmentActivity implements OnMapRe
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        LatLng latitudeLongitude = new LatLng(-19.893156213517234, -43.93205951184109);
-
-        mMap.addMarker(new MarkerOptions().position(latitudeLongitude)
-                .title("Padaria FornoDouro")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.bakery_icone)));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latitudeLongitude, 15));
-
     }
 }
